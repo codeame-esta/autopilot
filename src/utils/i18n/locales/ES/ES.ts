04abbe6359eceb7ee2b91da082cadfe9c53263bf
IgnacioTomasRamallo <85121659+IgnacioTomasRamallo@users.noreply.github.com>
2026-05-28 19:05:45 -0300

    feat: translations securityCard (#6)

--- src/utils/i18n/locales/ES/ES.ts
+++ src/utils/i18n/locales/ES/ES.ts
@@ -3,89 +3,102 @@ export const SpanishLanguage = {
     COMMON: {
       SIDEBAR: {
         NAV_ITEMS: {
-          HOME: 'Inicio',
-          INSIGHTS: 'Perspectivas',
-          ACTIONS: 'Acciones',
-          TRANSACTIONS: 'Transacciones',
-          BUDGETS: 'Presupuestos',
-          GOALS: 'Metas',
-          SETTINGS: 'Configuración',
+          HOME: "Inicio",
+          INSIGHTS: "Perspectivas",
+          ACTIONS: "Acciones",
+          TRANSACTIONS: "Transacciones",
+          BUDGETS: "Presupuestos",
+          GOALS: "Metas",
+          SETTINGS: "Configuración",
         },
-        LOG_OUT: 'Cerrar sesión',
+        LOG_OUT: "Cerrar sesión",
       },
     },
   },
   PAGES: {
     DASHBOARD: {
-      AUTOPILOT_RUNNING: 'Autopilot en marcha',
-      GOOD_MORNING: '¡Buenos días, {{user}}!',
-      GOOD_AFTERNOON: '¡Buenas tardes, {{user}}!',
-      GOOD_EVENING: '¡Buenas noches, {{user}}!',
+      AUTOPILOT_RUNNING: "Autopilot en marcha",
+      GOOD_MORNING: "¡Buenos días, {{user}}!",
+      GOOD_AFTERNOON: "¡Buenas tardes, {{user}}!",
+      GOOD_EVENING: "¡Buenas noches, {{user}}!",
     },
     SETTINGS: {
-      TITLE: 'Configuración',
-      DESCRIPTION: 'Administra tu cuenta y preferencias',
-      PERSONAL_INFO: 'Información Personal',
+      TITLE: "Configuración",
+      DESCRIPTION: "Administra tu cuenta y preferencias",
+      PERSONAL_INFO: "Información Personal",
       PROFILE: {
-        TITLE: 'Perfil',
-        CHANGE_PHOTO: 'Cambiar foto',
+        TITLE: "Perfil",
+        CHANGE_PHOTO: "Cambiar foto",
         FIELDS: {
           FULL_NAME: {
-            LABEL: 'Nombre completo',
+            LABEL: "Nombre completo",
           },
           EMAIL: {
-            LABEL: 'Correo electrónico',
-          }
+            LABEL: "Correo electrónico",
+          },
         },
-        SUBMIT_BUTTON: 'Guardar cambios'
+        SUBMIT_BUTTON: "Guardar cambios",
       },
       APPEARANCE: {
-        TITLE: 'Apariencia',
-        SUB_TITLE: 'Personaliza la configuración de idioma y tema',
+        TITLE: "Apariencia",
+        SUB_TITLE: "Personaliza la configuración de idioma y tema",
         FIELDS: {
           LANGUAGE: {
-            LABEL: 'Lenguaje',
-            HELPER_TEXT: 'Seleccione su idioma preferido para la interfaz'
+            LABEL: "Lenguaje",
+            HELPER_TEXT: "Seleccione su idioma preferido para la interfaz",
           },
           THEME: {
-            LABEL: 'Tema',
+            LABEL: "Tema",
             OPTIONS: {
-              DARK: 'Oscuro',
-              LIGHT: 'Claro',
-              SYSTEM: 'Sistema'
+              DARK: "Oscuro",
+              LIGHT: "Claro",
+              SYSTEM: "Sistema",
             },
-            HELPER_TEXT: 'Elige cómo quieres que se vea Autopilot'
-          }
+            HELPER_TEXT: "Elige cómo quieres que se vea Autopilot",
+          },
         },
       },
       NOTIFICATIONS: {
-        TITLE: 'Notificaciones',
-        SUB_TITLE: 'Configura cómo recibes las notificaciones',
+        TITLE: "Notificaciones",
+        SUB_TITLE: "Configura cómo recibes las notificaciones",
         FIELDS: {
           PUSH: {
-            TITLE: 'Notificaciones Push',
-            SUB_TITLE: 'Reciba notificaciones en su dispositivo'
+            TITLE: "Notificaciones Push",
+            SUB_TITLE: "Reciba notificaciones en su dispositivo",
           },
           EMAIL: {
-            TITLE: 'Notificaciones Email',
-            SUB_TITLE: 'Reciba resúmenes semanales por correo electrónico'
+            TITLE: "Notificaciones Email",
+            SUB_TITLE: "Reciba resúmenes semanales por correo electrónico",
           },
           ANOMALY: {
-            TITLE: 'Alertas de anomalías',
-            SUB_TITLE: 'Reciba notificaciones cuando se detecte actividad inusual'
+            TITLE: "Alertas de anomalías",
+            SUB_TITLE: "Reciba notificaciones cuando se detecte actividad inusual",
           },
           GOAL: {
-            TITLE: 'Recordatorios de objetivos',
-            SUB_TITLE: 'Recibe actualizaciones sobre sus objetivos de ahorro'
-          }
-        }
+            TITLE: "Recordatorios de objetivos",
+            SUB_TITLE: "Recibe actualizaciones sobre sus objetivos de ahorro",
+          },
+        },
       },
       AUTOPILOT_SETTINGS: {
-        TITLE: 'Autopilot Settings',
-        SUB_TITLE: 'Configure AI automation preferences',
-
-      }
-
+        TITLE: "Ajustes Autopilot",
+        SUB_TITLE: "Configurar las preferencias de automatización de IA",
+      },
+      SECURITY: {
+        TITLE: "Seguridad",
+        SUB_TITLE: "Protege tu cuenta",
+        FIELDS: {
+          FACTOR: {
+            TITLE: "Autenticación de dos factores",
+            SUB_TITLE: "Añade una capa adicional de seguridad",
+          },
+          PASSWORD: {
+            TITLE: "Contraseña",
+            SUB_TITLE: "Última modificación hace 3 meses",
+            BUTTON: "Cambiar",
+          },
+        },
+      },
     },
   },
 };
