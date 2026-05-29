e8ca264c148111f6bb791d92349c9b24fafdd0e7
Ignacio Pedrosa <49048208+PedrosaIgnacio@users.noreply.github.com>
2026-05-29 16:01:06 -0300

    feat: add change theme function, and build theme provider (#11)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Router from './router/router.tsx';
-import './i18n';
-
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Router from "./router/router.tsx";
+import "./i18n";
+import { ThemeProvider } from "./components/common/theme-provider.tsx";
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <Router />
-  </QueryClientProvider>
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
+  </QueryClientProvider>,
 );
