e56791bc197610a57fff9b7d77d94deaad95c4cc
Ignacio Pedrosa <49048208+PedrosaIgnacio@users.noreply.github.com>
2026-05-29 14:59:26 -0300

    fix: updaet supabase redirect path (#9)

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -8,6 +8,9 @@ export default function LoginPage() {
     setIsLoading(true);
     await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/dashboard`,
+      },
     });
     setIsLoading(false);
   };
